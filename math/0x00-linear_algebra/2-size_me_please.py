0b86fae21f1ce2ddfb983e69498e28237a741210
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-01 01:10:01 -0500

    add pycodestyle

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,10 +3,11 @@
     task 3: Shape of a matrix
 """
 
+
 def matrix_shape(matrix):
     """
     Returns the shape of a matrix
     """
     import numpy as np
     new_matrix = np.array(matrix)
-    return list(new_matrix.shape)
+    return list(new_matrix.ndim)
