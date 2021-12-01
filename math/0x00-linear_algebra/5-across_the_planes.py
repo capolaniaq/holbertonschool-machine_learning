24956c86fda883034ef685805c58f1589d6bd831
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-01 13:22:05 -0500

    5-across_the_planes.py

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Task 5:
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+        Adds two matrices
+    """
+    import numpy as np
+    mat1 = np.matrix(mat1)
+    mat2 = np.matrix(mat2)
+    if mat1.shape != mat2.shape:
+        return None
+    add_matrix = np.add(mat1, mat2)
+    return add_matrix
