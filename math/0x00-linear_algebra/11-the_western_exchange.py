0f1f6d98c4785d0bbfe978977022ed3c03bdf254
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 02:49:35 -0500

    11-the_western_exchange.py

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Task 11: The Western Exchange
+"""
+
+
+def np_transpose(matrix):
+    """
+    Function that transposes a matrix.
+    """
+    return matrix.transpose()
