ffed61f02c0a60439871b375bf8513e720196ba6
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-01 20:45:41 -0500

    8-ridin_bareback.py

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Task 8: Ridin' Bareback
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+        Function that multiplies two matrices
+    """
+    import numpy as np
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
+    if mat1.shape[1] != mat2.shape[0]:
+        return None
+    else:
+        return np.matmul(mat1, mat2)
