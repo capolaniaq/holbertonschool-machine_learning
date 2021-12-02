f7cb28fbf93a041ca634fb1d23a3c5be85fb4b2a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 17:18:29 -0500

    7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -17,6 +17,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     elif axis == 1:
         if mat1.shape[0] != mat2.shape[0]:
             return None
-    elif axis > 2:
+    elif mat1.shape[0] == 0 or mat1.shape[1] == 0:
         return None
+    
     return np.concatenate((mat1, mat2), axis=axis).tolist()
