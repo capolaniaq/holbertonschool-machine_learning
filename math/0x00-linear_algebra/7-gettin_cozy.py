112ce585e61f656ba4aeb7d0bcd2a59b5c96927c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 16:07:16 -0500

    fixing the shebang

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,4 +9,4 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Function that Concatenates two matrices along a specific axis
     """
     import numpy as np
-    return np.concatenate((mat1, mat2), axis=axis).tolist()
+    return np.ma.concatenate((mat1, mat2), axis=axis).tolist()
