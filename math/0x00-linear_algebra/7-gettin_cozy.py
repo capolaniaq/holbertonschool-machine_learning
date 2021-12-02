6bc580880896506dd7bc4b30b48713b6870727e2
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 17:11:56 -0500

    7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -11,6 +11,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     import numpy as np
     mat1 = np.array(mat1)
     mat2 = np.array(mat2)
-    if mat1.shape == 0 or mat2.shape == 0:
-        return None
+    if axis == 0:
+        if mat1.shape[1] != mat2.shape[1]:
+            return None
+    elif axis == 1:
+        if mat1.shape[0] != mat2.shape[0]:
+            return None
     return np.concatenate((mat1, mat2), axis=axis).tolist()
