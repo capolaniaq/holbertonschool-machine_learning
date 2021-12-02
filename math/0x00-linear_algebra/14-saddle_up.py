4d371ad383609973a2ee3243515f287130374514
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 15:09:35 -0500

    14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Task 14: Saddle up
+"""
+
+
+def np_matmul(mat1, mat2):
+    """
+        Function that performs matrix multiplication on two matrices
+    """
+    import numpy as np
+    return np.dot(mat1, mat2)
