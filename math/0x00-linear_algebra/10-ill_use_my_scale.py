069ae99ae623da1a0bcda2076a4b708e04eac311
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 02:45:18 -0500

    10-ill_use_my_scale.py

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Task 10: ill use my scale
+"""
+
+
+def np_shape(matrix):
+    """
+        Returns the shape of a numpy.ndarray
+    """
+    import numpy as np
+    matrix = np.array(matrix)
+    return matrix.shape
