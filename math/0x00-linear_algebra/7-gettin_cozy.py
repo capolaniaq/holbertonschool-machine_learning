bb1be381172e09c18a2f32a5a9180a5379cfc8b9
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-02 16:12:07 -0500

    conditional shape matrices

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,4 +9,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Function that Concatenates two matrices along a specific axis
     """
     import numpy as np
-    return np.ma.concatenate((mat1, mat2), axis=axis).tolist()
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
+    if mat1.shape != mat2.shape:
+        return None
+    return np.concatenate((mat1, mat2), axis=axis).tolist()
