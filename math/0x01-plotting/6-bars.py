6437b7e184869b8750dcb7a1e7304a804758e5a7
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-12 23:39:20 -0500

    6-bars.py

---
+++ math/0x01-plotting/6-bars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4,3))
+
+x = ['Farrah', 'Fred', 'Felicia']
+
+plt.bar(x, fruit[0], color='r', width=0.5)
+plt.bar(x, fruit[1], bottom=fruit[0], color='y', width=0.5)
+plt.bar(x, fruit[2], bottom=fruit[0]+fruit[1], color='#ff8000', width=0.5)
+plt.bar(x, fruit[3], bottom=fruit[0]+fruit[1]+fruit[2], color='#ffe5b4', width=0.5)
+
+
+
+plt.ylabel('Quantity of Fruit')
+plt.ylim([0, 80])
+plt.title('Number of Fruit per Person')
+
+plt.legend(['apples', 'bananas','oranges','peaches'], loc='upper right')
+plt.show()
