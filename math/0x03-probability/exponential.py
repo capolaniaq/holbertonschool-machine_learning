33ea4421da661f332d69972adb38b36e5bbb0992
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-15 02:41:19 -0500

    exponential.py

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Exponencial distribution
+"""
+
+
+class Exponential:
+    """
+        Class Exponencial
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+            Constructor
+        """
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = float(1/(sum(data) / len(data)))
