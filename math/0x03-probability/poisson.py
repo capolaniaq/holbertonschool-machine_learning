72406a424072759fa0e50da91b07bf3ea30b0df8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-15 01:15:21 -0500

    fixing the float number for lambtha variable

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -15,7 +15,7 @@ class Poisson:
         if data is None:
             if lambtha <= 0:
                 raise ValueError("Lambtha must be a positive value")
-            self.lambtha = lambtha
+            self.lambtha = float(lambtha)
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
