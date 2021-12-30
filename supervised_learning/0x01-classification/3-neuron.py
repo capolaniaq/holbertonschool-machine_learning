c384606c2d7cbc70b0b331d6a956856e38bd7745
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-30 12:34:28 -0500

    3-neuron.py

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+class neuron:
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    class neuron
+    """
+
+    def __init__(self, nx):
+        """
+        constructor class neuron
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for A
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        """
+        cost = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()
+        return cost
+
