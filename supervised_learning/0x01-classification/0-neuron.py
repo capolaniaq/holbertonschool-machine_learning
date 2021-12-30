9c0cca9ff9403f562b75290f44c51d8ce470d978
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-12-30 01:08:05 -0500

    directory supervised_learning

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env  python3
+"""
+class neuron:
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    class neuron
+    """
+
+    def __init__(self, nx):
+        """
+        constructor class neuron
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
