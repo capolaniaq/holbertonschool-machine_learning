0cdf550b44c01692ad2b56195aec4e06fa6c5b36
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-03 00:23:29 -0500

    16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -19,16 +19,20 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if len(layers) == 0 or type(layers) is not list:
-            raise TypeError("layers must be a list of positive integers")
         if any(list(map(lambda x: x <= 0, layers))):
             raise TypeError('layers must be a list of positive integers')
+        if len(layers) == 0 or type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for i in range(self.L):
+            key = "W{}".format(i + 1)
             if i == 0:
-                self.weights["W1"] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.weights["W1"] = np.random.randn(layers[i],
+                                                     nx) * np.sqrt(2/nx)
             else:
-                self.weights["W" + str(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+                square = np.sqrt(2 / layers[i - 1])
+                self.weights[key] = np.random.randn(layers[i],
+                                                    layers[i - 1]) * square
             self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
