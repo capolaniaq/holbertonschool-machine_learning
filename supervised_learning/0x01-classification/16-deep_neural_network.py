5951fee6cf1ce760f0a8415105e40cad07e81610
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-02 23:53:44 -0500

    16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -27,7 +27,5 @@ class DeepNeuralNetwork:
         for i in range(self.L):
             if type(layers[i]) is not int or layers[i] < 0:
                 raise TypeError("layers must be a list of positive integers")
-            key = "W{}".format(i + 1)
-            self.weights[key] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            key = "b{}".format(i + 1)
-            self.weights[key] = np.zeros((layers[i - 1], layers[i]))
+            self.weights["W" + str(i + 1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
