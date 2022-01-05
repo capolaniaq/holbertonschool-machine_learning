5a231cad41a16fabd93b468ad9eeaa0f6065a94c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-04 19:47:14 -0500

    3-neuron.py

--- supervised_learning/0x01-classification/3-neuron.py
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -55,5 +55,5 @@ class Neuron:
         """
         Calculates the cost of the model using logistic regression
         """
-        cost = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()
+        cost = -1/A.shape[1] * np.sum((Y * np.log(A)) + ((1 - Y)*(np.log(1.0000001 - A))))
         return cost
