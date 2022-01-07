b60ddf8e1fe87f42ccc4c7721582b6f9c4777b29
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-06 19:47:08 -0500

    3-calculate_accuracy.py

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -9,6 +9,4 @@ def calculate_accuracy(y, y_pred):
     """
     Calculate ocurracy function
     """
-    correct = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-    return accuracy
+    return tf.metrics.accuracy(y, y_pred)
