2ffca192ed73081fda126dde10941b3079e23763
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-12 13:31:26 -0500

    2-shuffle_data.py

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -9,9 +9,7 @@ def shuffle_data(X, Y):
     """
     shuffles the data points in two matrices
     """
-    m, nx = X.shape
-    permutation = np.random.permutation(m)
-    print(permutation)
-    shuffled_X = X[permutation, :]
-    shuffled_Y = Y[permutation, :]
+    permutation = np.random.permutation(X.shape[0])
+    shuffled_X = X[permutation]
+    shuffled_Y = Y[permutation]
     return shuffled_X, shuffled_Y
