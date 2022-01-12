d3542ccf08bf732a2e8d8136b68f7386b70f237d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-12 18:53:50 -0500

    2-shuffle_data.py

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -9,5 +9,7 @@ def shuffle_data(X, Y):
     """
     shuffles the data points in two matrices
     """
+    if (X.shape[0] != Y.shape[0]):
+        return None
     p = np.random.permutation(X.shape[0])
     return X[p], Y[p]
