60df4497e4e6e2bf01ca7018e1e4efe00bdc1c44
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-17 02:04:47 -0500

    13-batch_norm.py

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -8,13 +8,16 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    normalizes an unactivated output of a neural network using batch normalization:
+    normalizes an unactivated output of a neural network using
+    batch normalization:
 
     Z is a numpy.ndarray of shape (m, n) that should be normalized
     m is the number of data points
     n is the number of features in Z
-    gamma is a numpy.ndarray of shape (1, n) containing the scales used for batch normalization
-    beta is a numpy.ndarray of shape (1, n) containing the offsets used for batch normalization
+    gamma is a numpy.ndarray of shape (1, n) containing the scales
+    used for batch normalization
+    beta is a numpy.ndarray of shape (1, n) containing the
+    offsets used for batch normalization
     epsilon is a small number used to avoid division by zero
     Returns: the normalized Z matrix
     """
