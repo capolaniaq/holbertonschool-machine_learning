1e19ed586718e9255fbf0fc7abf4814809450448
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-23 11:52:47 -0500

    3-l2_reg_create_layer.py

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Create a layer with L2 regularization
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Create a tensorflow layer with L2 regularization
+    """
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode=("fan_avg"))
+    kernel_regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=initializer,
+                            kernel_regularizer=kernel_regularizer,
+                            name='layer')
+    return layer(prev)
