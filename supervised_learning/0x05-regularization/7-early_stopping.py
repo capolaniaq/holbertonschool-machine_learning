908c1f903b61f494b785f542e20c60b1858990c1
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-24 02:53:26 -0500

    7-early_stopping.py

---
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Early stopping
+"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    cost is the current validation cost of the neural network
+    opt_cost is the lowest recorded validation cost of the neural
+    network
+    threshold is the threshold used for early stopping
+    patience is the patience count used for early stopping
+    count is the count of how long the threshold has not been met
+    Returns: a boolean of whether the network should be stopped early,
+    followed by the updated count
+    """
+    if cost < opt_cost - threshold:
+        count = 0
+    else:
+        count += 1
+    if count == patience:
+        return True, count
+    return False, count
