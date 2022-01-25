39dc98e9355806bb610889333d3fb16ce5a98278
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-25 02:41:36 -0500

    1-input.py

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -17,19 +17,19 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     keep_prob is the probability that a node will be kept for dropout
     You are not allowed to use the Sequential class
     """
-    input = Keras.Input(shape=(nx, ), name='input_1')
+    input = Keras.Input(shape=(nx, ))
     L2 = Keras.regularizers.L2(lambtha)
     for i in range(len(layers)):
         if i == 0:
             x = Keras.layers.Dense(layers[i], activation=activations[i],
-                                   kernel_regularizer=L2, name='dense')(input)
+                                   kernel_regularizer=L2)(input)
         elif i == len(layers) - 1:
             x = Keras.layers.Dropout(1 - keep_prob)(x)
             output = Keras.layers.Dense(layers[i], activation=activations[i],
-                                        kernel_regularizer=L2, name='dense_' + str(i))(x)
+                                        kernel_regularizer=L2)(x)
         else:
             x = Keras.layers.Dropout(1 - keep_prob)(x)
             x = Keras.layers.Dense(layers[i], activation=activations[i],
-                                   kernel_regularizer=L2, name='dense_' + str(i))(x)
+                                   kernel_regularizer=L2)(x)
     model = Keras.Model(inputs=input, outputs=output)
     return model
