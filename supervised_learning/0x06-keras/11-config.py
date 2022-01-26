e3162c2c8d2f63ad87a904ce3f1cec01c5415eb8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-25 19:08:59 -0500

    11-config.py

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -18,6 +18,7 @@ def save_config(network, filename):
     with open(filename, 'w') as file:
         file.write(config)
 
+
 def load_config(filename):
     """
     loads a model with a specific configuration:
