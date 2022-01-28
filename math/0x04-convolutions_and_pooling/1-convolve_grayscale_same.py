f813d7170810d74ce0c46a9a10137d3480b2ea04
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-27 19:44:23 -0500

    1-convolve_grayscale_same.py

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -8,19 +8,27 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+    images is a numpy.ndarray with shape (m, h, w)
+    containing multiple grayscale images
         m is the number of images
         h is the height in pixels of the images
         w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+    kernel is a numpy.ndarray with shape (kh, kw)
+    containing the kernel for the convolution
         kh is the height of the kernel
         kw is the width of the kernel
     if necessary, the image should be padded with 0’s
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    pad = (kw - 1) // 2
-    images_pad = np.pad(images, ((0, 0), (pad, pad), (pad, pad)), 'constant', constant_values=0)
+    pad_h = kh - 1 // 2
+    pad_w = kw - 1 // 2
+    if kw % 2 == 0:
+        pad_w += 1
+    if kh % 2 == 0:
+        pad_h += 1
+    images_pad = np.pad(images, ((0, 0), (pad_h, pad_h),
+                        (pad_w, pad_w)), 'constant')
     conv = np.zeros((m, h, w))
 
     total_images = np.arange(m)
@@ -31,4 +39,3 @@ def convolve_grayscale_same(images, kernel):
                                         i:i + kh, j:j + kw] * kernel,
                                         axis=(1, 2)))
     return conv
-
