ff2ae52096422fad63933d9668683d525a224e25
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-01-31 15:21:23 -0500

    6-pool.py

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -7,6 +7,22 @@ import numpy as np
 
 
 def pool(images, kernel_shape, stride, mode='max'):
+    """
+    images is a numpy.ndarray with shape (m, h, w, c) containing multiple images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+        c is the number of channels in the image
+    kernel_shape is a tuple of (kh, kw) containing the kernel shape for the pooling
+        kh is the height of the kernel
+        kw is the width of the kernel
+    stride is a tuple of (sh, sw)
+        sh is the stride for the height of the image
+        sw is the stride for the width of the image
+    mode indicates the type of pooling
+    max indicates max pooling
+    avg indicates average pooling
+    """
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
