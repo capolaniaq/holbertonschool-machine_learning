eec4094d9d6e3a1921c5f2c7b8e464f0a92e1744
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-02-25 17:25:52 -0500

    0-yolo.py

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Object Detection project
+"""
+
+import numpy as np
+import tensorflow.keras as K
+
+
+class Yolo:
+    """
+    Class Yolo
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Initialization  the Yolo class
+        """
+        self.class_t = class_t
+        self.model = K.models.load_model(model_path)
+        self.nms_t = nms_t
+        self.anchors = anchors
+        with open(classes_path) as f:
+            for class_name in f.readlines():
+                self.class_names = class_name.strip()
---
+++ supervised_learning/0x0A-object_detection/README.md
@@ -0,0 +1,22 @@
+# 0x0A. Object Detection
+
+Author: Carlos Andres Polania (capolaniaq@correo.udistrital.edu.co)
+
+## Tasks
+
+### 0. Initialize Yolo
+
+### 1. Process Outputs
+
+### 2. Filter Boxes
+
+### 3. Non-max Suppression
+
+### 4. Load images
+
+### 5. Preprocess images
+
+### 6. Show boxes
+
+### 7. Predict
+
