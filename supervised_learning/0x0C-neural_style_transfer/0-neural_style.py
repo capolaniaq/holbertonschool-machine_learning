86f8234a784d4a14c6d723be249f67e86cdb904c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-02 11:23:30 -0500

    0-neural_style.py

---
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Class NST
+"""
+
+import tensorflow as tf
+import numpy as np
+
+
+class NST:
+    """
+    class NST
+    """
+    style_layers = ['block1_conv1', 'block2_conv1',
+                    'block3_conv1', 'block4_conv1', 'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """
+        Constructor
+        """
+        if not isinstance(style_image, np.ndarray) or\
+           len(style_image.shape) != 3 or\
+           style_image.shape[2] != 3:
+            raise TypeError('style_image must be a numpy.ndarray \
+                            with shape (h, w, 3)')
+
+        if not isinstance(content_image, np.ndarray) or\
+           len(content_image.shape) != 3 or\
+           content_image.shape[2] != 3:
+            raise TypeError('content_image must be a numpy.ndarray \
+                            with shape (h, w, 3)')
+
+        if alpha < 0:
+            raise TypeError('alpha must be a non-negative number')
+
+        if beta < 0:
+            raise TypeError('beta must be a non-negative number')
+
+        tf.compat.v1.enable_eager_execution()
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+
+        @staticmethod
+        def scale_image(image):
+            """
+            image - a numpy.ndarray of shape (h, w, 3) containing
+            the image to be scaled
+            if image is not a np.ndarray with the shape (h, w, 3),
+            raise a TypeError with the message image must be a
+            numpy.ndarray with shape (h, w, 3)
+            The scaled image should be a tf.tensor with the shape
+            (1, h_new, w_new, 3) where max(h_new, w_new) == 512 and
+            min(h_new, w_new) is scaled proportionately
+            The image should be resized using bicubic interpolation
+            After resizing, the image’s pixel values should be
+            rescaled from the range [0, 255] to [0, 1].
+            Returns: the scaled image
+            """
+            if not isinstance(image, np.ndarray) or\
+                len(image.shape) != 3 or\
+                    image.shape[2] != 3:
+                raise TypeError('image must be a numpy.ndarray \
+                                with shape (h, w, 3)')
+            if image.shape[0] > image.shape[1]:
+                new_h = 512
+                new_w = int(new_h * image.shape[1] / image.shape[0])
+            else:
+                new_w = 512
+                new_h = int(new_w * image.shape[0] / image.shape[1])
+            image = np.expand_dims(image, axis=0)
+            image = tf.compat.v1.image.resize_bicubic(image, (new_h, new_w))
+            image = tf.cast(image, tf.float32)
+            image = tf.compat.v1.divide(image, 255)
+            Image = np.clip(Image, 0, 1)
+            return image
---
+++ supervised_learning/0x0C-neural_style_transfer/README.md
@@ -0,0 +1,27 @@
+# 0x0C. Neural Style Transfer
+
+Author: Carlos Andres Polania(capolaniaq@correo.udistrital.edu.co)
+
+## Tasks
+
+### 0. Initialize
+
+### 1. Load the Model
+
+### 2. Gram Matrix
+
+### 3. Extract Features
+
+### 4. Layer Style Cost
+
+### 5. Style Cost
+
+### 6. Content Cost
+
+### 7. Total Cost
+
+### 8. Compute Gradients
+
+### 9. Generate Image
+
+### 10. Variational Cost
