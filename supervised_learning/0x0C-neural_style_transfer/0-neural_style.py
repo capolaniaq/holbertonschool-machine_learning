a21b966a10590c1e6a50c4f539bd459db1e9595d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-02 18:58:51 -0500

    0-neural_style.py

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -21,20 +21,16 @@ class NST:
         """
         error1 = "style_image must be a numpy.ndarray with shape (h, w, 3)"
         error2 = "content_image must be a numpy.ndarray with shape (h, w, 3)"
-        if type(style_image) is not np.ndarray or\
-           len(style_image.shape) != 3 or\
-           style_image.shape[2] != 3:
+        if type(style_image) is not np.ndarray or len(style_image.shape) != 3 or style_image.shape[2] != 3:
             raise TypeError(error1)
 
-        if type(content_image) is not np.ndarray or\
-           len(content_image.shape) != 3 or\
-           content_image.shape[2] != 3:
+        if type(content_image) is not np.ndarray or len(content_image.shape) != 3 or content_image.shape[2] != 3:
             raise TypeError(error2)
 
-        if alpha < 0:
+        if type(alpha) is not int and type(alpha) is not float or alpha < 0:
             raise TypeError('alpha must be a non-negative number')
 
-        if beta < 0:
+        if type(beta) is not int and type(beta) is not float or beta < 0:
             raise TypeError('beta must be a non-negative number')
 
         tf.enable_eager_execution()
@@ -60,9 +56,7 @@ class NST:
         Returns: the scaled image
         """
         error = 'image must be a numpy.ndarray with shape (h, w, 3)'
-        if type(image) is not np.ndarray or\
-           len(image.shape) != 3 or\
-           image.shape[2] != 3:
+        if type(image) is not np.ndarray or len(image.shape) != 3 or image.shape[2] != 3:
             raise TypeError(error)
         h, w, c = image.shape
         if h > w:
