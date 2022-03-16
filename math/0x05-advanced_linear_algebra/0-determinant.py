8b521e15d5c8ab0fd56148040f7d5cd2ed11b94e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-16 16:05:09 -0500

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -3,13 +3,11 @@
 Module that calculate a determinant form matrix
 """
 
-import numpy as np
-
-
 def determinant(matrix):
     """
     Function that calculate a determinant
     """
+    import numpy as np
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
     for i in matrix:
