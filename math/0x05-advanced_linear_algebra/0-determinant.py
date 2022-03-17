6e820164947e955613463f3f6cfbeac32892c919
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-16 22:25:09 -0500

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -6,7 +6,7 @@ Module that calculate a determinant form matrix
 
 def determinant(matrix):
     """Function that calculate a determinant from matrix"""
-    if type(matrix) is not list or len(matrix) == 0:
+    if type(matrix) is not list or type(matrix[0]) is not list:
         raise TypeError("matrix must be a list of lists")
 
     if len(matrix) == 1 and len(matrix[0]) == 0:
