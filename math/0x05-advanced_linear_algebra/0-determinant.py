6a3f36270d200e166fc32f56d917082dbe3683d3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-17 14:17:44 -0500

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -4,19 +4,10 @@ Module that calculate a determinant form matrix
 """
 
 
-def minor(matrix, row, col):
-    """Function that calculate a minor from matrix"""
-    minor = []
-    for i in range(len(matrix)):
-        if i != row:
-            minor.append([])
-            for j in range(len(matrix[i])):
-                if j != col:
-                    minor[-1].append(matrix[i][j])
-    return minor
+from xml.dom.minidom import Element
+
 
 def determinant(matrix):
-    """Function that calculate a determinant from matrix"""
     if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
 
@@ -32,11 +23,16 @@ def determinant(matrix):
     if len(matrix) == 1:
         return matrix[0][0]
 
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    n = len(matrix)
 
-    determinant = 0
-    if len(matrix) > 2:
-        for i in range(len(matrix)):
-            determinant += matrix[0][i] * determinant(minor(matrix, 0, i)) * (-1) ** i
-    return determinant
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            factor = matrix[j][i] / matrix[i][i]
+            for k in range(n):
+                matrix[j][k] = matrix[j][k] - (factor * matrix[i][k])
+
+    determinant = 1
+    for i in range(n):
+        determinant *= matrix[i][i]
+
+    return int(determinant)
