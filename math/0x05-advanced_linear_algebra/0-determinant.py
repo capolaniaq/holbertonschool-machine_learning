30b87575dcbe2258e87272ed1f0a8c5c9e1598f0
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-16 19:57:59 -0500

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -29,4 +29,11 @@ def determinant(matrix):
                 determinant = determinant - (value * ((matrix[1][0] * matrix[2][2]) - (matrix[1][2] * matrix[2][0])))
             else:
                 determinant = determinant + (value * ((matrix[1][0] * matrix[2][1]) - (matrix[1][1] * matrix[2][0])))
+
+    if len(matrix) > 3:
+        for i, row in enumerate(matrix):
+            if i == 0:
+                determinant = determinant + (row[0] * ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1])))
+            else:
+                determinant = determinant - (row[0] * ((matrix[0][1] * matrix[2][2]) - (matrix[0][2] * matrix[2][1])))
     return determinant
