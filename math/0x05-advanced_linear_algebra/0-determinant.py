867b3f367d21efc2ad8a82b837af905b9698c85f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-16 21:08:15 -0500

    0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -3,37 +3,38 @@
 Module that calculate a determinant form matrix
 """
 
+
 def determinant(matrix):
     """Function that calculate a determinant from matrix"""
     if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+
     for row in matrix:
-        if len(matrix) == 1 and len(row) == 0:
-            return 1
-        if type(row) is not list:
-            raise TypeError("matrix must be a list of lists")
         if len(matrix) != len(row):
             raise ValueError("matrix must be a square matrix")
+
     if len(matrix) == 1:
         return matrix[0][0]
 
     if len(matrix) == 2:
-        for i, row in enumerate(matrix):
-            return (row[i] * matrix[1][i + 1]) - (row[i + 1] * matrix[1][i])
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
     determinant = 0
     if len(matrix) == 3:
         for i, value in enumerate(matrix[0]):
             if i == 0:
-                determinant = (value * ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1])))
+                a = matrix[1][1] * matrix[2][2]
+                b = matrix[1][2] * matrix[2][1]
+                determinant = (value * (a - b))
             elif i == 1:
-                determinant = determinant - (value * ((matrix[1][0] * matrix[2][2]) - (matrix[1][2] * matrix[2][0])))
+                a = matrix[1][0] * matrix[2][2]
+                b = matrix[1][2] * matrix[2][0]
+                determinant = determinant - (value * (a - b))
             else:
-                determinant = determinant + (value * ((matrix[1][0] * matrix[2][1]) - (matrix[1][1] * matrix[2][0])))
-
-    if len(matrix) > 3:
-        for i, row in enumerate(matrix):
-            if i == 0:
-                determinant = determinant + (row[0] * ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1])))
-            else:
-                determinant = determinant - (row[0] * ((matrix[0][1] * matrix[2][2]) - (matrix[0][2] * matrix[2][1])))
+                a = matrix[1][0] * matrix[2][1]
+                b = matrix[1][1] * matrix[2][0]
+                determinant = determinant + (value * (a - b))
     return determinant
