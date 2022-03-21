34493e097df5a5324388767ac2dec8fe4b173f4b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-21 17:36:37 -0500

    multinormal.py

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Class MultivariateNormal
+"""
+
+import numpy as np
+
+
+class MultiNormal:
+    """Class Multinormal"""
+
+    def __init__(self, data):
+        """Constructor Class"""
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean = np.mean(data, axis=1).reshape(data.shape[0], 1)
+        X_mean = data - self.mean
+        self.cov = np.matmul(X_mean, X_mean.T) / (data.shape[1] - 1)
