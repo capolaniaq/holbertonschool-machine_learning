8360ac2be9f3887e903298410d3fd3ba7b9c1956
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-21 16:48:30 -0500

    0-mean_cov.py

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Calculate the mean and covariance matrix of a data set:
+"""
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    X is a numpy array with shape (n, d) containing the data set:
+    n is the number of data points
+    d is the number of dimensions in each data point
+    return mean, covariance
+    """
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=(0))
+    X_mean = X - mean
+    cov = np.matmul(X_mean.T, X_mean) / (n - 1)
+    return mean, cov
---
+++ math/0x06-multivariate_prob/README.md
@@ -0,0 +1,20 @@
+# 0x06. Multivariate Probability
+
+Author: Carlos Andres Polania (capolaniaq@correo.udistrital.edu.co)
+
+## Tasks
+
+### 0. Mean and Covariance
+
+Write a function `def mean_cov(X):` that calculates the mean and covariance of a data set:
+
+### 1. Correlation
+
+Write a function `def correlation(C):` that calculates a correlation matrix:
+
+### 2. Initialize
+
+Create the class `MultiNormal` that represents a Multivariate Normal distribution:	
+
+### 3. PDF
+Update the class `MultiNormal`:
