e20fb36a0bfbd3277ef9a94f12b90fbac5430e9c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-03-29 02:29:04 -0500

    1-pca.py

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -14,7 +14,8 @@ def pca(X, ndim):
     Returns: T, a numpy.ndarray of shape (n, ndim)
     containing the transformed version of X
     """
-    U, S, V = np.linalg.svd(X)
-    W = V.T[:, : ndim]
-    T = np.matmul(X, W)
+    X_m = X - np.mean(X, axis=0)
+    U, Sigma, Vr = np.linalg.svd(X_m)
+    W = Vr.T[:, :ndim]
+    T = np.matmul(X_m, W)
     return T
