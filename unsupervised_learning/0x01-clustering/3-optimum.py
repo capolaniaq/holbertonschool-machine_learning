60bef378c01a592e39d91c736066c9e52f1bee91
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-01 18:48:14 -0500

    3-optimum.py

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -23,17 +23,17 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         d_vars is a list containing the difference in variance
         from the smallest cluster size for each cluster size
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if type(kmin) is not int or kmin <= 0:
+    if not isinstance(iterations, int) or iterations < 1:
         return None, None
-    if kmax is not None and (type(kmax) is not int or kmax <= 0):
+    if kmax is not None and (type(kmax) is not int or kmax < 1):
         return None, None
-    if type(kmax) is not int or kmax <= 0:
+    if kmax is not None and kmin >= kmax:
         return None, None
-    if kmin >= kmax:
-        return None, None
-    if type(iterations) is not int or iterations <= 0:
+    if kmax is None:
+        kmax = X.shape[0]
+    if not isinstance(kmin, int) or kmin < 1 or kmin >= X.shape[0]:
         return None, None
 
     results = []
