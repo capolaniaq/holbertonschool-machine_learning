fc007f4665812989df35c084abc4010862069d80
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-15 02:02:37 -0500

    0-gp.py

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -25,6 +25,6 @@ class GaussianProcess:
         """
         Calculate the covariance matrix
         """
-        sum_2 = np.sum(X2**2, 1) - 2*np.dot(X1, X2.T)
+        sum_2 = np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + sum_2
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
