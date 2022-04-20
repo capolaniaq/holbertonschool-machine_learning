a900b05d3097e2492cd61c292671f12f3c8dbf70
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-20 14:08:09 -0500

    0-vanilla.py

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -23,36 +23,16 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     All layers should use a relu activation except for the last layer in the
     decoder, which should use sigmoid
     """
-    X_input_encoded = keras.Input(shape=(input_dims,))
-    hidden_ly = keras.layers.Dense(units=hidden_layers[0], activation='relu')
-
-    # reversed for the decoder
-    Y_prev = hidden_ly(X_input_encoded)
-    for i in range(1, len(hidden_layers)):
-        hidden_ly = keras.layers.Dense(units=hidden_layers[i],
-                                       activation='relu')
-        Y_prev = hidden_ly(Y_prev)
-
-    latent_ly = keras.layers.Dense(units=latent_dims, activation='relu')
-    bottleneck = latent_ly(Y_prev)
-    encoder = keras.Model(X_input_encoded, bottleneck)
-
-    X_input_decoded = keras.Input(shape=(latent_dims,))
-    hidden_ly_decoded = keras.layers.Dense(units=hidden_layers[-1],
-                                           activation='relu')
-    Y_prev = hidden_ly_decoded(X_input_decoded)
-    for j in range(len(hidden_layers) - 2, -1, -1):
-        hidden_ly_decoded = keras.layers.Dense(units=hidden_layers[j],
-                                               activation='relu')
-        Y_prev = hidden_ly_decoded(Y_prev)
-    last_layer = keras.layers.Dense(units=input_dims, activation='sigmoid')
-    output = last_layer(Y_prev)
-    decoder = keras.Model(X_input_decoded, output)
-
-    X_input = keras.Input(shape=(input_dims,))
-    encoder_o = encoder(X_input)
-    decoder_o = decoder(encoder_o)
-    auto = keras.Model(X_input, decoder_o)
-    auto.compile(loss='binary_crossentropy', optimizer='adam')
-
+    input_layer = keras.Input(shape=(input_dims,))
+    hidden_layers = [input_layer] + [keras.layers.Dense(units=layer,
+                                                        activation='relu')
+                                        for layer in hidden_layers]
+    encoder = keras.models.Sequential(hidden_layers)
+    decoder_layers = [keras.layers.Dense(units=layer, activation='relu')
+                        for layer in reversed(hidden_layers[1:])]
+    decoder_layers.append(keras.layers.Dense(units=input_dims,
+                                                activation='sigmoid'))
+    decoder = keras.models.Sequential(decoder_layers)
+    auto = keras.models.Sequential([encoder, decoder])
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
     return encoder, decoder, auto
