a54b07b87c1b34fd92a8935114c23fec58692837
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-04-28 23:39:21 -0500

    7-bi_output.py

---
+++ supervised_learning/0x0D-RNNs/7-bi_output.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+class BidirectionalCell:
+"""
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    class BidirectionalCell that Represents a bidirectional cell of an RNN
+    """
+
+    def __init__(self, i, h, o):
+        """
+        i is the dimensionality of the data
+        h is the dimensionality of the hidden states
+        o is the dimensionality of the outputs
+        Creates the public instance attributes Whf, Whb, Wy, bhf, bhb,
+        by that represent the weights and biases of the cell
+        Whf and bhfare for the hidden states in the forward direction
+        Whb and bhbare for the hidden states in the backward direction
+        Wy and byare for the outputs
+        The weights should be initialized using a random normal distribution
+        in the order listed above
+        The weights will be used on the right side for matrix multiplication
+        The biases should be initialized as zeros
+        """
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(2 * h, o)
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        x_t is a numpy.ndarray of shape (m, i) that contains the data input
+        for the cell
+        m is the batch size for the data
+        h_prev is a numpy.ndarray of shape (m, h) containing the previous
+        hidden state
+        Returns: h_next, the next hidden state
+        """
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concat, self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """
+        x_t is a numpy.ndarray of shape (m, i) that contains the data
+        input for the cell
+        m is the batch size for the data
+        h_next is a numpy.ndarray of shape (m, h) containing the next
+        hidden state
+        Returns: h_pev, the previous hidden state
+        """
+        concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.matmul(concat, self.Whb) + self.bhb)
+        return h_prev
+
+    def output(self, H):
+        """
+        H is a numpy.ndarray of shape (t, m, 2 * h) that contains the
+        concatenated hidden states from both directions, excluding
+        their initialized states
+        t is the number of time steps
+        m is the batch size for the data
+        h is the dimensionality of the hidden states
+        Returns: Y, the outputs
+        """
+        Y = np.matmul(H, self.Wy) + self.by
+        return Y
