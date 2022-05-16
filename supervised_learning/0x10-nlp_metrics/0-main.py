f4a2f7702e6ecfa1e7c1d89c8438f99264c67424
Carlos Andrés Polania Quijano <66897395+capolaniaq@users.noreply.github.com>
2022-05-15 23:54:24 -0500

    delete 0-main.py

--- supervised_learning/0x10-nlp_metrics/0-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-uni_bleu = __import__('0-uni_bleu').uni_bleu
-
-references = [["the", "cat", "is", "on", "the", "mat"], ["there", "is", "a", "cat", "on", "the", "mat"]]
-sentence = ["there", "is", "a", "cat", "here"]
-
-print(uni_bleu(references, sentence))
