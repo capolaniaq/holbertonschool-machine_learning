8d5814ea120c5d868fbb5bd82dccec7f1c5ec0d5
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-15 23:40:02 -0500

    0-uni_bleu.py

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -9,20 +9,38 @@ import numpy as np
 def uni_bleu(references, sentence):
     """
     Calculate the unigram BLEU score.
-    :param references: a list of reference sentences
-    :param sentence: a hypothesis sentence
-    :return: the unigram BLEU score
+    param references: a list of reference sentences
+    param sentence: a hypothesis sentence
+    return: the unigram BLEU score
     """
-    # Calculate the length of the hypothesis sentence.
-    hyp_len = len(sentence)
+    list_references = list(set(sentence))
 
-    # Calculate the unigram BLEU score.
-    score = 0.
-    for ref in references:
-        ref_len = len(ref)
-        # Calculate the length of the common sub-sequence.
-        lcs = len(np.intersect1d(sentence, ref))
-        score += lcs / (hyp_len + ref_len - lcs)
-    score /= len(references)
+    count_references = {}
 
-    return score
+    for reference in references:
+        for word in reference:
+            if word in list_references:
+                if word not in count_references:
+                    count_references[word] = reference.count(word)
+                else:
+                    if reference.count(word) > count_references[word]:
+                        count_references[word] = reference.count(word)
+                    else:
+                        pass
+
+    lenght_refrence = []
+    for reference in references:
+        lenght_refrence.append(len(reference))
+
+    r = min(lenght_refrence)
+
+    c = len(list_references)
+
+    if c > r:
+        bp = 1
+    else:
+        bp = np.exp(1 - r / c)
+
+    blue = bp * np.exp(np.log(sum(count_references.values()) / c))
+
+    return blue
