f36d9ab6551d740e0ba6b803823d856d9a4db7d6
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-20 18:48:05 -0500

    0-rnn_encoder.py

---
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Create a class RNNEncoder
+"""
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
+
+
+class RNNEncoder(Layer):
+    """
+    Class RNNEncoder
+    """
+
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Constructor class:
+            vocab: is an integer representing the size of the output vocabulary
+            embedding: is an integer representing the dimensionality of the
+                embedding vector
+            units: is an integr representing the number of hidden units in the
+                RNN cell
+            batch: is an integer representing the batch size
+        """
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = Layer.GRU(units, return_sequences=True,
+                                return_state=True, recurrent_initializer='glorot_uniform')
+        super(RNNEncoder, self).__init__()
+
+    def initialize_hidden_state(self):
+        """
+        Initialize the hidden state for the RNN
+        """
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        """
+        x: is a tensor of shape (batch, input_seq_len, d)
+        initial: is the initial hidden state
+        """
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
---
+++ supervised_learning/0x11-attention/README.md
@@ -0,0 +1,37 @@
+# 0x11. Attention
+
+## Author: Carlos Andres Polania (capolaniaq@correo.udistrital.edu.co)
+
+-   What is the attention mechanism?
+-   How to apply attention to RNNs
+-   What is a transformer?
+-   How to create an encoder-decoder transformer model
+-   What is GPT?
+-   What is BERT?
+-   What is self-supervised learning?
+-   How to use BERT for specific NLP tasks
+-   What is SQuAD? GLUE?
+## Tasks
+
+### 0. RNN Encoder
+
+### 1. Self Attention
+
+### 2. RNN Decoder
+
+### 3. Positional Encoding
+
+### 4. Scaled Dot Product Attention
+
+### 5. Multi Head Attention
+
+### 6. Transformer Encoder Block
+
+### 7. Transformer Decoder Block
+
+### 8. Transformer Encoder
+### 9. Transformer Decoder
+
+### 10. Transformer Network
+
+
