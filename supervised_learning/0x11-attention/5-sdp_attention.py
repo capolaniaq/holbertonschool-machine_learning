188cddb8dd2ea104e16732c12c3bbaecf3971b3e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-05-22 04:21:37 -0500

    5-sdp_attention.py

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Scaled Dot Product Attention
+"""
+
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Calculate the attention weights
+    q, k, v must have matching leading dimensions.
+    k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+    The mask has different shapes depending on its type(padding or look ahead)
+    but it must be broadcastable for addition.
+
+      Args:
+    q: query shape == (..., seq_len_q, depth)
+    k: key shape == (..., seq_len_k, depth)
+    v: value shape == (..., seq_len_v, depth_v)
+    mask: Float tensor with shape broadcastable
+          to (..., seq_len_q, seq_len_k). Defaults to None.
+
+    Returns:
+        output, attention_weights
+    """
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * (-1e9))
+
+    weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(weights, V)
+
+    return output, weights
