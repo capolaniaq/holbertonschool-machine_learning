a641de9856d490aee66cf600d239d9f76dc8c295
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-25 09:44:28 -0500

    0-passengers.py

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Request the ships with passeger"""
+
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Function that make a get request to StarWars Api ships
+    and select someone had a equal or bigger passegers
+
+    Args:
+        passengetCount is the integer of passegers to check
+    Return:
+        list of the ships with had conditions oor list empty
+    """
+    response = requests.get("https://swapi-api.hbtn.io/api/starships/")
+    response = response.json()
+    ships = []
+    next = None
+
+    while(response['next'] is not None):
+        if next is not None:
+            response = requests.get(next)
+            response = response.json()
+
+        for i, ship in enumerate(response['results']):
+            try:
+                if passengerCount <= int(ship['passengers']):
+                    ships.append(ship['name'])
+            except:
+                if ship['passengers'] != 'n/a' or \
+                            ship['passengers'] != 'unknown':
+                    ships.append(ship['name'])
+        next = response['next']
+    return ships
