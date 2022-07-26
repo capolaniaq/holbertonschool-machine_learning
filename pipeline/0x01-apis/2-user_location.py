438a219124e4c7c1f56e947eeb9b023eae41f520
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-25 23:19:20 -0500

    2-user_location.py

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     response = requests.get(url)
     if response.status_code == 404:
         print("Not found")
-    elif response.status_code == 403:
+    if response.status_code == 403:
         init = int(response.headers['X-Ratelimit-Reset'])
         lim = int(time.time())
         X = int((lim - init) / 60)
         print("Reset in {} min".format(int(X)))
-    elif response.status_code == 200:
+    if response.status_code == 200:
         response = response.json()
         print(response['location'])
