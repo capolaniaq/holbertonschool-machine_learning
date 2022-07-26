9a5d370810f06883dcf56c092d1c112f86eb2bb6
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-25 23:15:25 -0500

    2-user_location.py

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -9,14 +9,14 @@ import time
 if __name__ == '__main__':
 
     url = sys.argv[1]
-    response = requests.get(url, {'Accept': 'application/vnd.github.v3+json'})
+    response = requests.get(url, headers={'Accept': 'application/vnd.github.v3+json'})
     if response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
         init = int(response.headers['X-Ratelimit-Reset'])
         lim = int(time.time())
         X = int((lim - init) / 60)
-        print(f"Reset in {X} min")
+        print("Reset in {} min".format(X))
     elif response.status_code == 200:
         response = response.json()
         print(response['location'])
