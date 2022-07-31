74e13b29438847f714607470aace1dfdb4ea7588
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2022-07-31 10:21:09 -0500

    8-genre_id_by_show.sql

--- pipeline/0x02-databases/8-genre_id_by_show.sql
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
